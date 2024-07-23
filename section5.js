ed58bff50b70e9c7dd9a4786cf05f666d3ea9290
ki3ico <158471668+tnkKmk@users.noreply.github.com>
2024-07-23 22:16:43 +0900

    first commit

---
+++ section5.js
@@ -0,0 +1,13 @@
+function plus(n1, n2) {
+  return n1 + n2;
+}
+
+function f3(a, b) {
+  if (a === "test" && b !== "temp") {
+    return b;
+  } else {
+    return " " + b + "random text";
+  }
+}
+
+var totalnumberOf_catIn_tokyo_atLastYear = 0;
